648b054c1091164383f71262f2a69ff21d9f8dcd
OmarSalama17 <o.salama.abdallah@gmail.com>
2025-11-20 21:25:02 +0200

    pagination

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -8,8 +8,9 @@ import Filter from "@/Components/Filter";
 
 const page =  () => {
     const [selectedStatus , setSelectedStatus] = useState<filterCategory>(true)
+  const [pagination , setPagination]  = useState<Number>(1)
 
-  const {isError , isLoading , error , data} = useGetProducts(selectedStatus );
+  const {isError , isLoading , error , data} = useGetProducts(selectedStatus , pagination );
 
 if(isLoading){
   return <div>loading</div>
@@ -23,6 +24,11 @@ if(isError){
         <h1>products</h1>
         <Filter selectedStatus={selectedStatus} setSelectedStatus={setSelectedStatus}/>
             <ProductList products={data as Product[]} />
+            <div className="flex justify-center">
+              <button onClick={() =>  setPagination(1)} className="p-4 rounded-md bg-amber-50 text-black m-2 cursor-pointer">1</button>
+              <button onClick={() =>  setPagination(2)} className="p-4 rounded-md bg-amber-50 text-black m-2 cursor-pointer">2</button>
+              <button onClick={() =>  setPagination(3)} className="p-4 rounded-md bg-amber-50 text-black m-2 cursor-pointer">3</button>
+            </div>
     </div>
   )
 }
--- hooks/useGetProducts.ts
+++ hooks/useGetProducts.ts
@@ -1,16 +1,31 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import axios from "axios"
+import axios from "axios";
 import { Product } from "../types/product";
 import { filterCategory } from "@/types/filter";
 
+const fetchProducts = async (
+  selectedStatus: filterCategory,
+  pagination:Number
+): Promise<Product[]> => {
+//   const res = await axios.get(`https://68f816d9deff18f212b51c45.mockapi.io/api/product/${selectedStatus ? `?isActive=${selectedStatus}` : ""}`
+  const res = await axios.get(
+    `https://68f816d9deff18f212b51c45.mockapi.io/api/product?page=${pagination}&limit=10`
+  );
+  if (res.status !== 200) {
+    throw new Error("Failed to fetch products");
+  }
 
-const fetchProducts = async (selectedStatus:filterCategory) :Promise<Product[]> => {
-    const res = await axios.get(`https://68f816d9deff18f212b51c45.mockapi.io/api/product/${selectedStatus ? `?isActive=${selectedStatus}` : ''}`);
-    
-    return res.data as Product[]
-}
-const useGetProducts = (selectedStatus :filterCategory):UseQueryResult<Product[] , Error> => {
-    return useQuery<Product[] , Error>({queryKey: ['products' , selectedStatus], queryFn: ()=> fetchProducts(selectedStatus)})
-}
+  return res.data as Product[];
+};
+const useGetProducts = (
+  selectedStatus: filterCategory , pagination:Number
+): UseQueryResult<Product[], Error> => {
+  return useQuery<Product[], Error>({
+    queryKey: ["products", selectedStatus , pagination],
+    queryFn: () => fetchProducts(selectedStatus , pagination),
+    // staleTime: 1000 * 5,
+    // refetchInterval: 1000 * 6,
+  });
+};
 
-export default useGetProducts
+export default useGetProducts;
